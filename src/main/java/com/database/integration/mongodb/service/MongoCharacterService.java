109305d676a03e5a17d69dc851767f7b4a8ea3ac
Tomasz Jasinski <tomjas@gmail.com>
2024-03-18 17:15:05 +0100

    Minor fix

--- src/main/java/com/database/integration/mongodb/service/MongoCharacterService.java
+++ src/main/java/com/database/integration/mongodb/service/MongoCharacterService.java
@@ -3,10 +3,12 @@ package com.database.integration.mongodb.service;
 import com.database.integration.mongodb.model.MonogCharacter;
 import com.database.integration.mongodb.repository.MongoCharacterRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Service
 public class MongoCharacterService {
 
     private MongoCharacterRepository characterRepository;
